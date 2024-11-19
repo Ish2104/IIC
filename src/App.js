6e835d0311ea300c852104fb7b5722f8834047ea
Sushant-4444 <sushant23537@iiitd.ac.in>
2024-11-19 21:51:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -25,6 +25,29 @@ import mem9 from "./images/aman.png";
 
 
 
+import OncosImage from './images/Oncos.png'
+import InternImage from './images/Intern.png';
+import DesignImage from './images/Design.png';
+import DesImage from './images/Des.png';
+import EpochImage from './images/Epoch.png';
+import EnactusImage from './images/Enactus.png';
+import CapImage from './images/Cap.png';
+import RobImage from './images/Rob.png';
+import ForkImage from './images/Fork.png';
+import BizImage from './images/Biz.png';
+import FinImage from './images/Fin.png';
+import SolvImage from './images/Solv.png';
+import FintechImage from './images/Fintech.png';
+import VcsImage from './images/Vcs.png';
+import BharatImage from './images/Bharat.png';
+import AdImage from './images/Ad.png';
+import Afro from './images/Afro.png';
+import Wit from './images/Wit.png';
+import Micro from './images/Micro.png';
+import Cam from './images/Cam.png';
+
+
+
 
 function App() {
 
@@ -42,14 +65,66 @@ const teamMembers2 = [
   { name: "Kumar Gurusahai", img: mem8, linkedin: "https://www.linkedin.com/in/kumar-gurusahai-075209241/" },
   { name: "Aman Ranjan", img: mem9, linkedin: "https://www.linkedin.com/in/aman-ranjan-a8853320b/" }
 ];
+
+const eventsData1 = [
+  { id: 1, title: 'OnCoslice', date: '15th March 2024', image: OncosImage, pdf: '/documents/OnCoslice.pdf' },
+  { id: 2, title: 'Internship Fair', date: '15-16 March 2024', image: InternImage, pdf: '/documents/Internship.pdf' },
+  { id: 3, title: 'Design Panel', date: '16 March 2024', image: DesignImage, pdf: '/documents/Design.pdf' },
+  { id: 4, title: 'Design Hub Designathon', date: '16 March 2024', image: DesImage, pdf: '/documents/Designathon.pdf' },
+  { id: 5, title: 'Epoch', date: '15-16 March 2024', image: EpochImage, pdf: '/documents/Epoch.pdf' },
+  { id: 6, title: 'Enactus fair', date: '16 March 2024', image: EnactusImage, pdf: '/documents/Enactus.pdf' },
+  { id: 7, title: 'Capitalist Date', date: '16 March 2024', image: CapImage, pdf: '/documents/Cap.pdf' },
+  { id: 8, title: 'Robosoccer', date: '16 March 2024', image: RobImage, pdf: '/documents/Rob.pdf' },
+  { id: 9, title: 'Fork It', date: '16 March 2024', image: ForkImage, pdf: '/documents/Fork.pdf' },
+  { id: 10, title: 'The Biz-Ent-ine Empire', date: '15 March 2024', image: BizImage, pdf: '/documents/Biz.pdf' },
+  { id: 11, title: 'Finance Dilema', date: '16 March 2024', image: FinImage, pdf: '/documents/Fin.pdf' },
+  { id: 12, title: 'Bidding B-Plan', date: '16 March 2024', image: SolvImage, pdf: '/documents/Bid.pdf' },
+  { id: 13, title: 'Fintech Hackathon', date: '15-16 March 2024', image: FintechImage, pdf: '/documents/Fintech.pdf' },
+  { id: 14, title: 'Vcs Investor Panel', date: '15 March 2024', image: VcsImage, pdf: '/documents/Vcs.pdf' },
+  { id: 15, title: 'Bharat AI', date: '16 March 2024', image: BharatImage, pdf: '/documents/Bharat.pdf' },
+  { id: 16, title: 'Ad-Mad', date: '15 March 2024', image: AdImage, pdf: '/documents/Ad.pdf' },
+
+/* 
+  nodata
+  { id: 17, title: 'IIITD Pitchers S1', date: '5 April 2024', image: , pdf: '' },
+  { id: 18, title: 'IIITD Pitchers S2', date: '12 April 2024', image: , pdf: '' },
+  { id: 19, title: 'IIITD Pitchers S3', date: '17 April 2024', image: , pdf: '' },
+
+  { id: 20, title: 'World Environment Day ', date: '5 June 2024', image: , pdf: '' },
+*/
+  
+
+  { id: 17, title: 'Afro Asian Conference', date: '6 July 2024', image: Afro, pdf: '/documents/Afro.pdf' },
+  { id: 18, title: 'Witnessing Disaster', date: '6-10 August 2024', image: Wit, pdf: '/documents/Wit.pdf' },
+  { id: 19, title: 'Micro-eco of Irrigation', date: '9 August 2024', image: Micro, pdf: '/documents/Micro.pdf' },
+  { id: 20, title: 'Campus Investment', date: '9 August 2024', image: Cam, pdf: '/documents/Cam.pdf' },
+
+  /*   
+   nodata
+  { id: 21, title: 'Snackchat Event ', date: '21 August 2024', image: , pdf: '' },
+  { id: 22, title: 'TechSpectra 2024 ', date: '23-25 August 2024', image: AdImage, pdf: '' },
+  { id: 23, title: 'Vision Alignment and Team Building Workshop ', date: '17 September 2024', image: , pdf: '' },
+  { id: 24, title: 'Smart India Hackathon', date: '21 September 2024', image: , pdf: '' },
+  { id: 25, title: 'India Internet Day', date: '27 September 2024', image: , pdf: '' },
+  */
+
+
+];
+
+const eventsData2 = [
+  { id: 1, title: 'OnCoslice', date: '15th March 2024', image: OncosImage, pdf: '/documents/OnCoslice.pdf' },
+  { id: 2, title: 'Internship Fair', date: '15-16 March 2024', image: InternImage, pdf: '/documents/Internship.pdf' },
+  { id: 3, title: 'Design Panel', date: '16 March 2024', image: DesignImage, pdf: '/documents/Design.pdf' },
+  { id: 4, title: 'Design Hub Designathon', date: '16 March 2024', image: DesImage, pdf: '/documents/Designathon.pdf' }
+];
   return (
     <div className="App">
       <Header />
       {/* <HerosSection /> */}
       <Main />
       <Services />
-      
-      <InE />
+      <InE eventsData={eventsData2} heading="Current Events"/>
+      <InE eventsData={eventsData1} heading="Innovation & Entrepreneurship Events"/>
       <Major />
       <Team teamMembers={teamMembers1} teamType="FACULTY" />
       <Team teamMembers={teamMembers2} teamType="STUDENT" />
--- src/components/Events/InE.jsx
+++ src/components/Events/InE.jsx
@@ -23,52 +23,52 @@ import Cam from '../../images/Cam.png';
 
 import './InE.css';
 
-const eventsData = [
-  { id: 1, title: 'OnCoslice', date: '15th March 2024', image: OncosImage, pdf: '/documents/OnCoslice.pdf' },
-  { id: 2, title: 'Internship Fair', date: '15-16 March 2024', image: InternImage, pdf: '/documents/Internship.pdf' },
-  { id: 3, title: 'Design Panel', date: '16 March 2024', image: DesignImage, pdf: '/documents/Design.pdf' },
-  { id: 4, title: 'Design Hub Designathon', date: '16 March 2024', image: DesImage, pdf: '/documents/Designathon.pdf' },
-  { id: 5, title: 'Epoch', date: '15-16 March 2024', image: EpochImage, pdf: '/documents/Epoch.pdf' },
-  { id: 6, title: 'Enactus fair', date: '16 March 2024', image: EnactusImage, pdf: '/documents/Enactus.pdf' },
-  { id: 7, title: 'Capitalist Date', date: '16 March 2024', image: CapImage, pdf: '/documents/Cap.pdf' },
-  { id: 8, title: 'Robosoccer', date: '16 March 2024', image: RobImage, pdf: '/documents/Rob.pdf' },
-  { id: 9, title: 'Fork It', date: '16 March 2024', image: ForkImage, pdf: '/documents/Fork.pdf' },
-  { id: 10, title: 'The Biz-Ent-ine Empire', date: '15 March 2024', image: BizImage, pdf: '/documents/Biz.pdf' },
-  { id: 11, title: 'Finance Dilema', date: '16 March 2024', image: FinImage, pdf: '/documents/Fin.pdf' },
-  { id: 12, title: 'Bidding B-Plan', date: '16 March 2024', image: SolvImage, pdf: '/documents/Bid.pdf' },
-  { id: 13, title: 'Fintech Hackathon', date: '15-16 March 2024', image: FintechImage, pdf: '/documents/Fintech.pdf' },
-  { id: 14, title: 'Vcs Investor Panel', date: '15 March 2024', image: VcsImage, pdf: '/documents/Vcs.pdf' },
-  { id: 15, title: 'Bharat AI', date: '16 March 2024', image: BharatImage, pdf: '/documents/Bharat.pdf' },
-  { id: 16, title: 'Ad-Mad', date: '15 March 2024', image: AdImage, pdf: '/documents/Ad.pdf' },
+// const eventsData = [
+//   { id: 1, title: 'OnCoslice', date: '15th March 2024', image: OncosImage, pdf: '/documents/OnCoslice.pdf' },
+//   { id: 2, title: 'Internship Fair', date: '15-16 March 2024', image: InternImage, pdf: '/documents/Internship.pdf' },
+//   { id: 3, title: 'Design Panel', date: '16 March 2024', image: DesignImage, pdf: '/documents/Design.pdf' },
+//   { id: 4, title: 'Design Hub Designathon', date: '16 March 2024', image: DesImage, pdf: '/documents/Designathon.pdf' },
+//   { id: 5, title: 'Epoch', date: '15-16 March 2024', image: EpochImage, pdf: '/documents/Epoch.pdf' },
+//   { id: 6, title: 'Enactus fair', date: '16 March 2024', image: EnactusImage, pdf: '/documents/Enactus.pdf' },
+//   { id: 7, title: 'Capitalist Date', date: '16 March 2024', image: CapImage, pdf: '/documents/Cap.pdf' },
+//   { id: 8, title: 'Robosoccer', date: '16 March 2024', image: RobImage, pdf: '/documents/Rob.pdf' },
+//   { id: 9, title: 'Fork It', date: '16 March 2024', image: ForkImage, pdf: '/documents/Fork.pdf' },
+//   { id: 10, title: 'The Biz-Ent-ine Empire', date: '15 March 2024', image: BizImage, pdf: '/documents/Biz.pdf' },
+//   { id: 11, title: 'Finance Dilema', date: '16 March 2024', image: FinImage, pdf: '/documents/Fin.pdf' },
+//   { id: 12, title: 'Bidding B-Plan', date: '16 March 2024', image: SolvImage, pdf: '/documents/Bid.pdf' },
+//   { id: 13, title: 'Fintech Hackathon', date: '15-16 March 2024', image: FintechImage, pdf: '/documents/Fintech.pdf' },
+//   { id: 14, title: 'Vcs Investor Panel', date: '15 March 2024', image: VcsImage, pdf: '/documents/Vcs.pdf' },
+//   { id: 15, title: 'Bharat AI', date: '16 March 2024', image: BharatImage, pdf: '/documents/Bharat.pdf' },
+//   { id: 16, title: 'Ad-Mad', date: '15 March 2024', image: AdImage, pdf: '/documents/Ad.pdf' },
 
-/* 
-  nodata
-  { id: 17, title: 'IIITD Pitchers S1', date: '5 April 2024', image: , pdf: '' },
-  { id: 18, title: 'IIITD Pitchers S2', date: '12 April 2024', image: , pdf: '' },
-  { id: 19, title: 'IIITD Pitchers S3', date: '17 April 2024', image: , pdf: '' },
+// /* 
+//   nodata
+//   { id: 17, title: 'IIITD Pitchers S1', date: '5 April 2024', image: , pdf: '' },
+//   { id: 18, title: 'IIITD Pitchers S2', date: '12 April 2024', image: , pdf: '' },
+//   { id: 19, title: 'IIITD Pitchers S3', date: '17 April 2024', image: , pdf: '' },
 
-  { id: 20, title: 'World Environment Day ', date: '5 June 2024', image: , pdf: '' },
-*/
+//   { id: 20, title: 'World Environment Day ', date: '5 June 2024', image: , pdf: '' },
+// */
   
 
-  { id: 17, title: 'Afro Asian Conference', date: '6 July 2024', image: Afro, pdf: '/documents/Afro.pdf' },
-  { id: 18, title: 'Witnessing Disaster', date: '6-10 August 2024', image: Wit, pdf: '/documents/Wit.pdf' },
-  { id: 19, title: 'Micro-eco of Irrigation', date: '9 August 2024', image: Micro, pdf: '/documents/Micro.pdf' },
-  { id: 20, title: 'Campus Investment', date: '9 August 2024', image: Cam, pdf: '/documents/Cam.pdf' },
+//   { id: 17, title: 'Afro Asian Conference', date: '6 July 2024', image: Afro, pdf: '/documents/Afro.pdf' },
+//   { id: 18, title: 'Witnessing Disaster', date: '6-10 August 2024', image: Wit, pdf: '/documents/Wit.pdf' },
+//   { id: 19, title: 'Micro-eco of Irrigation', date: '9 August 2024', image: Micro, pdf: '/documents/Micro.pdf' },
+//   { id: 20, title: 'Campus Investment', date: '9 August 2024', image: Cam, pdf: '/documents/Cam.pdf' },
 
-  /*   
-   nodata
-  { id: 21, title: 'Snackchat Event ', date: '21 August 2024', image: , pdf: '' },
-  { id: 22, title: 'TechSpectra 2024 ', date: '23-25 August 2024', image: AdImage, pdf: '' },
-  { id: 23, title: 'Vision Alignment and Team Building Workshop ', date: '17 September 2024', image: , pdf: '' },
-  { id: 24, title: 'Smart India Hackathon', date: '21 September 2024', image: , pdf: '' },
-  { id: 25, title: 'India Internet Day', date: '27 September 2024', image: , pdf: '' },
-  */
+//   /*   
+//    nodata
+//   { id: 21, title: 'Snackchat Event ', date: '21 August 2024', image: , pdf: '' },
+//   { id: 22, title: 'TechSpectra 2024 ', date: '23-25 August 2024', image: AdImage, pdf: '' },
+//   { id: 23, title: 'Vision Alignment and Team Building Workshop ', date: '17 September 2024', image: , pdf: '' },
+//   { id: 24, title: 'Smart India Hackathon', date: '21 September 2024', image: , pdf: '' },
+//   { id: 25, title: 'India Internet Day', date: '27 September 2024', image: , pdf: '' },
+//   */
 
 
-]; 
+// ]; 
 
-const EventCarousel = () => {
+const EventCarousel = (props) => {
   const settings = {
     dots: true,
     infinite: true,
@@ -108,17 +108,68 @@ const EventCarousel = () => {
 
   return (
     <div className="ine-events">
-      <h2 className="ine-title">Innovation & Entrepreneurship Events</h2>
+      <h2 className="ine-title">{props.heading}</h2>
       <div className="title_decorate"></div>
-      <Slider {...settings}>
-        {eventsData.map((event, index) => {
-          // Only create a new row every 3 events
+            {props.eventsData.length > 6 ? (
+        <Slider {...settings}>
+          {props.eventsData.map((event, index) => {
+            // Only create a new row every 6 events
+            if (index % 6 === 0) {
+              return (
+                <div key={index} className="ine-event-slide">
+                  <div className="ine-event-row">
+                    {/* Row 1: 3 events */}
+                    {props.eventsData.slice(index, index + 3).map(event => (
+                      <div key={event.id} className="ine-event-card">
+                        <div className="ine-event-image-container">
+                          <img src={event.image} alt={event.title} className="ine-event-image" />
+                        </div>
+                        <div className="ine-event-info">
+                          <h3>{event.title}</h3>
+                          <p>{event.date}</p>
+                          <div className="ine-event-buttons">
+                            <a href={event.pdf} className="ine-btn session-btn" target="_blank" rel="noopener noreferrer">
+                              Details
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="ine-event-row">
+                    {/* Row 2: 3 events */}
+                    {props.eventsData.slice(index + 3, index + 6).map(event => (
+                      <div key={event.id} className="ine-event-card">
+                        <div className="ine-event-image-container">
+                          <img src={event.image} alt={event.title} className="ine-event-image" />
+                        </div>
+                        <div className="ine-event-info">
+                          <h3>{event.title}</h3>
+                          <p>{event.date}</p>
+                          <div className="ine-event-buttons">
+                            <a href={event.pdf} className="ine-btn session-btn" target="_blank" rel="noopener noreferrer">
+                              Details
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            }
+            return null;
+          })}
+        </Slider>
+      ) : (
+        props.eventsData.map((event, index) => {
+          // Only create a new row every 6 events
           if (index % 6 === 0) {
             return (
               <div key={index} className="ine-event-slide">
                 <div className="ine-event-row">
                   {/* Row 1: 3 events */}
-                  {eventsData.slice(index, index + 3).map(event => (
+                  {props.eventsData.slice(index, index + 3).map(event => (
                     <div key={event.id} className="ine-event-card">
                       <div className="ine-event-image-container">
                         <img src={event.image} alt={event.title} className="ine-event-image" />
@@ -137,7 +188,7 @@ const EventCarousel = () => {
                 </div>
                 <div className="ine-event-row">
                   {/* Row 2: 3 events */}
-                  {eventsData.slice(index + 3, index + 6).map(event => (
+                  {props.eventsData.slice(index + 3, index + 6).map(event => (
                     <div key={event.id} className="ine-event-card">
                       <div className="ine-event-image-container">
                         <img src={event.image} alt={event.title} className="ine-event-image" />
@@ -158,8 +209,8 @@ const EventCarousel = () => {
             );
           }
           return null;
-        })}
-      </Slider>
+        })
+      )}
     </div>
   );
 };
